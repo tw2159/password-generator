179363503e96307a19d6e3bbe72fe89f2cd79f20
Te Wei <tw2159@nyu.edu>
2021-08-21 21:51:17 -0700

    character

--- Develop/script.js
+++ Develop/script.js
@@ -25,8 +25,39 @@ function getPasswordLength() {
   return passwordLength;
 }
 
+function getCharacters() {
+  var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numeric = "0123456789";
+  var special = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
 
 
+  var includeLowerCase = confirm("Would you like to include lowercase characters in your password?");
+  var includeUpperCase = confirm("Would you like to include uppercase characters in your password?");
+  var includeNumeric = confirm("Would you like to include numeric characters in your password?");
+  var includeSpecial = confirm("Would you like to include special characters in your password?");
+
+  if(includeLowerCase == true) {
+    characters += lowerCase;
+  }
+  if(includeUpperCase == true) {
+    characters += upperCase;
+  }
+  if(includeNumeric == true) {
+    characters += numeric;
+  }
+  if(includeSpecial == true) {
+    characters += special;
+  }
+
+  // Check to see if user selected none of the options (resulting in an empty string)
+  if(characters.length == 0) {
+    alert("You must select at least one of the four options!");
+    return getCharacters();
+  }
+
+  return characters;
+}
 
 
 // Add event listener to generate button
